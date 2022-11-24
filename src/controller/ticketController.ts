778d7be1a4cc00f6b0026cb768971d3949b214d0
biiit4894 <biit4894@sookmyung.ac.kr>
2022-11-24 15:58:58 +0900

    [CHORE] 404 메시지 추가

--- src/controller/ticketController.ts
+++ src/controller/ticketController.ts
@@ -4,6 +4,12 @@ import { ticketService } from "../service";
 // 메인 화면 조회
 const getTicket = async (req: Request, res: Response) => {
   const data = await ticketService.getTicket();
+
+  if (!data)
+    return res
+      .status(404)
+      .json({ status: 404, message: "메인 페이지 조회 실패" });
+
   return res
     .status(200)
     .json({ status: 200, message: "메인 페이지 조회 성공", data });
